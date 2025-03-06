6532570d04836450e23020bde18db85a474bc4da
JacksonMessa <jacksonadrianomessa@gmail.com>
2025-03-06 15:58:13 -0300

    Inverter String

---
+++ StringTarget/Program.cs
@@ -0,0 +1,8 @@
+﻿string original, inverso = "";
+Console.WriteLine("Digite uma frase:");
+original = Console.ReadLine();
+for(int i = original.Length-1; i >= 0; i--)
+{
+    inverso += original[i];
+}
+Console.WriteLine("String invertida: " + inverso);
